2a51d66dd8634390a24cf3055188e4cf113674a3
Jackson Meyer <jacksonmeyer@mmc.school.nz>
2022-06-15 12:56:42 +1200

    Random speed down the Game Panel

--- Practice-1/FrmDodge.cs
+++ Practice-1/FrmDodge.cs
@@ -15,6 +15,7 @@ namespace Practice_1
         Graphics g; //declare a graphics object called g
         // declare space for an array of 7 objects called planet 
         Planet[] planet = new Planet[7];
+        Random yspeed = new Random();
         public FrmDodge()
         {
             InitializeComponent();
@@ -33,6 +34,10 @@ namespace Practice_1
             //call the Planet class's DrawPlanet method to draw the image planet1 
             for (int i = 0; i < 7; i++)
             {
+                // generate a random number from 5 to 20 and put it in rndmspeed
+                int rndmspeed = yspeed.Next(5, 20);
+                planet[i].y += rndmspeed;
+
                 //call the Planet class's drawPlanet method to draw the images
                 planet[i].DrawPlanet(g);
             }
